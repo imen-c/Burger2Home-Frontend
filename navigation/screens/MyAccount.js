35be6779d99b551dcd978841f86cc1dc10be8e6a
imen <imen.cheref@letsgocity.be>
2023-01-03 15:27:57 +0100

    fix error import google auth

--- navigation/screens/MyAccount.js
+++ navigation/screens/MyAccount.js
@@ -1,12 +1,16 @@
-import * as React from 'react';
-import { View, Text } from 'react-native';
+import * as React from "react";
+import { View, Text } from "react-native";
+import * as Google from "expo-google-app-auth";
 
-export default function Home({navigation}) {
-    return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text
-            onPress={() => alert('MyB2h')}
-            style={{ fontSize: 26, fontWeight: 'bold' }}>MYB2H</Text>
+export default function Home({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text
+        onPress={() => alert("MyB2h")}
+        style={{ fontSize: 26, fontWeight: "bold" }}
+      >
+        MYB2H
+      </Text>
     </View>
-    );
-  }
+  );
+}
